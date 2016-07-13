7845221118962b6d1b8c07e8da12c2f445a905b1
Marcus Fihlon <marcus@fihlon.ch>
2016-07-13 16:47:15 +0200

    Started migration from DropWizard to Vert.x

---
+++ src/main/java/ch/fihlon/moodini/business/question/boundary/QuestionsVerticle.java
@@ -0,0 +1,153 @@
+/*
+ * Moodini
+ * Copyright (C) 2016 Marcus Fihlon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.fihlon.moodini.business.question.boundary;
+
+import ch.fihlon.moodini.business.question.control.QuestionService;
+import ch.fihlon.moodini.business.question.entity.Answer;
+import ch.fihlon.moodini.business.question.entity.Question;
+import ch.fihlon.moodini.business.user.control.UserService;
+import ch.fihlon.moodini.business.user.entity.User;
+import com.google.inject.Guice;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.util.Optional;
+
+public class QuestionsVerticle extends AbstractVerticle {
+
+    @Inject
+    private QuestionService questionService;
+
+    @Inject
+    private UserService userService;
+
+    @Override
+    public void start(Future<Void> fut) {
+        Guice.createInjector().injectMembers(this);
+
+        // Create a router object.
+        Router router = Router.router(vertx);
+
+        // Serve static resources from the assets directory
+        router.route("/assets/*").handler(StaticHandler.create("assets"));
+
+        router.route("/api/questions*").handler(BodyHandler.create());
+
+        router.post("/api/questions").handler(this::create);
+        router.get("/api/questions").handler(this::list);
+        router.get("/api/questions/latest").handler(this::latest);
+        router.get("/api/questions/:id").handler(this::read);
+        router.put("/api/questions/:id").handler(this::update);
+        router.delete("/api/questions/:id").handler(this::delete);
+        router.post("/api/questions/:id/vote").handler(this::vote);
+
+        // Create the HTTP server and pass the "accept" method to the request handler.
+        vertx
+                .createHttpServer()
+                .requestHandler(router::accept)
+                .listen(
+                        // Retrieve the port from the configuration,
+                        // default to 8080.
+                        config().getInteger("http.port", 8080),
+                        result -> {
+                            if (result.succeeded()) {
+                                fut.complete();
+                            } else {
+                                fut.fail(result.cause());
+                            }
+                        }
+                );
+    }
+
+    private void vote(RoutingContext routingContext) {
+        final Long id = Long.valueOf(routingContext.request().getParam("id"));
+        final String body = routingContext.getBodyAsString();
+        final Answer answer = Answer.valueOf(body);
+        questionService.vote(id, answer);
+        routingContext.response()
+                .end();
+    }
+
+    private void list(RoutingContext routingContext) {
+        routingContext.response()
+                .putHeader("Content-Type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(questionService.readAll()));
+    }
+
+    private void read(RoutingContext routingContext) {
+        final Long id = Long.valueOf(routingContext.request().getParam("id"));
+        final Optional<Question> question = questionService.read(id);
+        if (question.isPresent()) {
+            routingContext.response()
+                    .putHeader("Content-Type", "application/json; charset=utf-8")
+                    .end(Json.encodePrettily(question.get()));
+        } else {
+            routingContext.response()
+                    .setStatusCode(404)
+                    .end();
+        }
+    }
+
+    private void latest(RoutingContext routingContext) {
+        final Question question = questionService.readLatest();
+        routingContext.response()
+                .putHeader("Content-Type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(question));
+    }
+
+    private void create(RoutingContext routingContext) {
+        final String body = routingContext.getBodyAsString();
+        final Question question = Json.decodeValue(body,
+                Question.class);
+        final User user = userService.readAll().get(0);
+        final Question createdQuestion = questionService.create(user, question);
+        final String location = routingContext.normalisedPath() +
+                File.separator + createdQuestion.getQuestionId().toString();
+        routingContext.response()
+                .setStatusCode(201)
+                .putHeader("Location", location)
+                .end(Json.encodePrettily(createdQuestion));
+    }
+
+    private void update(RoutingContext routingContext) {
+        final Long id = Long.valueOf(routingContext.request().getParam("id"));
+        final Question question = Json.decodeValue(routingContext.getBodyAsString(),
+                Question.class).toBuilder().questionId(id).build();;
+        final User user = userService.readAll().get(0);
+        final Question updatedQuestion = questionService.update(user, question);
+        routingContext.response()
+                .putHeader("Content-Type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(updatedQuestion));
+    }
+
+    private void delete(RoutingContext routingContext) {
+        final Long id = Long.valueOf(routingContext.request().getParam("id"));
+        final User user = userService.readAll().get(0);
+        questionService.delete(user, id);
+        routingContext.response()
+                .setStatusCode(204);
+    }
+
+}
