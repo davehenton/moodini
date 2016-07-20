048e2483cf540013219863170cc5091e0eca443f
Marcus Fihlon <marcus@fihlon.ch>
2016-07-20 21:30:56 +0200

    Wrote a test for the Answer enum

---
+++ src/test/java/ch/fihlon/moodini/business/question/entity/AnswerTest.java
@@ -0,0 +1,19 @@
+package ch.fihlon.moodini.business.question.entity;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AnswerTest {
+
+    @Test
+    public void getAnswer() {
+        assertThat(Answer.AMPED.getAnswer(), is("Amped"));
+        assertThat(Answer.GOOD.getAnswer(), is("Good"));
+        assertThat(Answer.FINE.getAnswer(), is("Fine"));
+        assertThat(Answer.MEH.getAnswer(), is("Meh"));
+        assertThat(Answer.PISSED.getAnswer(), is("Pissed"));
+    }
+
+}
