f5cd4d73c2e18e7b1a5edd0a03ff81a3602986bd
Marcus Fihlon <marcus@fihlon.ch>
2016-07-22 11:03:15 +0200

    Refactored the unit test to use a mocked question service

--- src/test/java/ch/fihlon/moodini/business/question/boundary/QuestionsVerticleTest.java
+++ src/test/java/ch/fihlon/moodini/business/question/boundary/QuestionsVerticleTest.java
@@ -17,7 +17,10 @@
  */
 package ch.fihlon.moodini.business.question.boundary;
 
+import ch.fihlon.moodini.Injector;
+import ch.fihlon.moodini.business.question.control.QuestionService;
 import ch.fihlon.moodini.business.question.entity.Question;
+import com.google.inject.AbstractModule;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
@@ -30,20 +33,51 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.net.ServerSocket;
 
 import static com.jayway.restassured.RestAssured.port;
+import static java.util.Collections.singletonList;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(VertxUnitRunner.class)
 public class QuestionsVerticleTest {
 
+    private static final String QUESTION_TEXT = "Do you like testing?";
+    private static final Long QUESTION_ID = 1L;
+    private static final Long QUESTION_VERSION = 42L;
+    private static final String CONTENT_TYPE_APPLICATION_JSON = "application/json";
+    private static final String HEADER_CONTENT_TYPE = "Content-Type";
+    private static final String HEADER_CONTENT_LENGTH = "Content-Length";
+    private static final String HEADER_LOCATION = "Location";
+
     private Vertx vertx;
+
+    @Mock
+    private QuestionService questionServiceMock;
 
     @Before
     public void setUp(@NotNull final TestContext context) throws IOException {
+        final Question answerQuestion = Question.builder()
+                .question(QUESTION_TEXT)
+                .questionId(QUESTION_ID)
+                .version(QUESTION_VERSION)
+                .build();
+        questionServiceMock = mock(QuestionService.class);
+        when(questionServiceMock.create(any(Question.class))).thenReturn(answerQuestion);
+        when(questionServiceMock.readAll()).thenReturn(singletonList(answerQuestion));
+        Injector.setModule(new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(QuestionService.class).toInstance(questionServiceMock);
+            }
+        });
+
         vertx = Vertx.vertx();
         final ServerSocket socket = new ServerSocket(0);
         port = socket.getLocalPort();
@@ -56,27 +90,29 @@ public class QuestionsVerticleTest {
     @After
     public void tearDown(@NotNull final TestContext context) {
         vertx.close(context.asyncAssertSuccess());
+        Injector.resetModule();
     }
 
     @Test
     public void testCreate(@NotNull final TestContext context) {
         final Async async = context.async();
-        final String json = Json.encodePrettily(Question.builder().question("Do you like testing?").build());
+        final String json = Json.encodePrettily(Question.builder().question(QUESTION_TEXT).build());
         final String length = Integer.toString(json.length());
         vertx.createHttpClient().post(port, "localhost", "/api/questions")
-                .putHeader("Content-Type", "application/json")
-                .putHeader("Content-Length", length)
+                .putHeader(HEADER_CONTENT_TYPE, CONTENT_TYPE_APPLICATION_JSON)
+                .putHeader(HEADER_CONTENT_LENGTH, length)
                 .handler(response -> {
                     context.assertEquals(response.statusCode(), 201);
-                    context.assertTrue(response.headers().get("location").startsWith("/api/questions/"));
+                    context.assertTrue(response.headers().get(HEADER_LOCATION)
+                            .startsWith("/api/questions/"));
                     final MultiMap h = response.headers();
-                    final String ct = h.get("Content-Type");
-                    context.assertTrue(ct.contains("application/json"));
+                    final String ct = h.get(HEADER_CONTENT_TYPE);
+                    context.assertTrue(ct.contains(CONTENT_TYPE_APPLICATION_JSON));
                     response.bodyHandler(body -> {
                         final Question question = Json.decodeValue(body.toString(), Question.class);
-                        context.assertEquals(question.getQuestion(), "Do you like testing?");
-                        context.assertNotEquals(question.getQuestionId(), 0L);
-                        context.assertNotEquals(question.getVersion(), 0L);
+                        context.assertEquals(question.getQuestion(), QUESTION_TEXT);
+                        context.assertEquals(question.getQuestionId(), QUESTION_ID);
+                        context.assertEquals(question.getVersion(), QUESTION_VERSION);
                         async.complete();
                     });
                 })
@@ -91,9 +127,10 @@ public class QuestionsVerticleTest {
         vertx.createHttpClient().getNow(port, "localhost", "/api/questions",
                 response -> {
                     context.assertEquals(response.statusCode(), 200);
-                    context.assertTrue(response.headers().get("content-type").contains("application/json"));
+                    context.assertTrue(response.headers().get(HEADER_CONTENT_TYPE)
+                            .contains(CONTENT_TYPE_APPLICATION_JSON));
                     response.bodyHandler(body -> {
-                        context.assertTrue(body.toString().contains("Hallo Welt!"));
+                        context.assertTrue(body.toString().contains(QUESTION_TEXT));
                         async.complete();
                     });
                 });
