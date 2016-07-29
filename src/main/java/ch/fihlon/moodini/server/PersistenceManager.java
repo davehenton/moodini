6bc05d71351ca256350b9182a74de4933bc7f745
Marcus Fihlon <marcus@fihlon.ch>
2016-07-29 16:59:06 +0200

    Use Lombok to generate SLF4J logging facility

--- src/main/java/ch/fihlon/moodini/server/PersistenceManager.java
+++ src/main/java/ch/fihlon/moodini/server/PersistenceManager.java
@@ -18,8 +18,7 @@
 package ch.fihlon.moodini.server;
 
 import lombok.experimental.UtilityClass;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import pl.setblack.airomem.core.SimpleController;
 
 import java.io.Serializable;
@@ -27,6 +26,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.function.Supplier;
 
+@Slf4j
 @UtilityClass
 public class PersistenceManager {
 
@@ -35,8 +35,6 @@ public class PersistenceManager {
 
     private static final Path DATA_DIRECTORY;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PersistenceManager.class);
-
     static {
         DATA_DIRECTORY = Paths.get(MOODINI_DIRECTORY_NAME, PREVAYLER_DIRECTORY_NAME);
     }
@@ -44,7 +42,7 @@ public class PersistenceManager {
     public static <T extends Serializable> SimpleController<T> createSimpleController(
             final Class<? extends Serializable> clazz, final Supplier<T> constructor) {
         final String dir = DATA_DIRECTORY.resolve(clazz.getName()).toString();
-        LOGGER.info("Using persistence store '{}' for entity '{}'.",
+        log.info("Using persistence store '{}' for entity '{}'.",
                 Paths.get(System.getProperty("user.home"), dir).toAbsolutePath(),
                 clazz.getName());
         return SimpleController.loadOptional(dir, constructor);
